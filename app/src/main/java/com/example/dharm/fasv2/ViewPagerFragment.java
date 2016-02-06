5a3cd0e3d2add9ea9cbb345443c2f3fc1055abf4
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2016-02-06 14:04:30 -0800

    - Added a GridLayoutManager for the RecyclerView. - Dynamically generate the number of columns that will fit on the screen based on the size of the device.

--- app/src/main/java/com/example/dharm/fasv2/ViewPagerFragment.java
+++ app/src/main/java/com/example/dharm/fasv2/ViewPagerFragment.java
@@ -3,8 +3,9 @@ package com.example.dharm.fasv2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,20 +32,30 @@ public class ViewPagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // Dynamically generate the number of columns to have in the grid view based on the device
+        // the user has.
+        float scaleFactor = getResources().getDisplayMetrics().density * 100;
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int columns = (int) ((float) displayMetrics.widthPixels / scaleFactor) / 2;
+        if (columns == 0 || columns == 1) {
+            columns = 2;
+        }
+
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.view_pager_fragment, container, false);
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(
                 R.id.recycler_view_in_fragment);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
-                getActivity().getBaseContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity().getBaseContext(),
+                columns);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
         recyclerView.setHasFixedSize(true);
 
         ArrayList<DoodleData> doodleDataList = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 150; i++) {
             DoodleData temp = new DoodleData("omega", "some really long title", "omega2", "omega3",
                     "omega4", "omega5");
             doodleDataList.add(temp);
